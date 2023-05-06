e0ca8e0b86001263e93197dfc132efbbd29c611a
Ousmane Cissé <cisseousmane134@gmail.com>
2023-05-05 22:02:34 -0400

    more about module concepts

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const names = require('./name')
 const sayHi = require('./utils')
+const data = require('./alternative-flavor')
+require('./mind-grenade')
+
+console.log(data)
 
 sayHi(names.john)
 sayHi(names.peter)
